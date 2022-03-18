fa089143cf0e3f7f4c8e354f7981d709d7e44edc
Nicholas Saarela <nicholas.saarela@gmail.com>
2022-03-18 23:41:22 +0200

    added drawController and related service

--- services/whiteboard.service.js
+++ services/whiteboard.service.js
@@ -6,6 +6,14 @@ const notifyUserAboutRequest = (userId, whiteboardId, decision) => {
   console.log(`User ${userId} is notified about the ${decision ? 'positive' : 'negative'} decision to join whiteboard ${whiteboardId}`)
 }
 
+const leaveSession = (userId, whiteboardId) => {
+  console.log(`User ${userId} is notified that he has left whiteboard ${whiteboardId}`)
+}
+
+const kickUser = (userId, whiteboardId) => {
+  console.log(`Host is notified if user ${userId} was successfully kicked`)
+}
+
 module.exports = {
   askHostToJoin,
   notifyUserAboutRequest
