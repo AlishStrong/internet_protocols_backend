5628f5c94e281d200cdd005cac3fe10144c1a784
Anton Sulkava <anton.sulkava@aalto.fi>
2022-03-21 23:57:50 +0200

    UC 7 editing the stickynote updates to DB

--- controllers/edit.contoller.js
+++ controllers/edit.contoller.js
@@ -2,7 +2,7 @@ const editRouter = require('express').Router()
 const mongoose = require('mongoose')
 const Whiteboard = require('../models/whiteboard.model')
 const Element = require('../models/element.model')
-const { WHITEBOARD_DOES_NOT_EXIST, UNKNOWN_ACTIONID, } = require('../utils/error.constants')
+const { WHITEBOARD_DOES_NOT_EXIST, UNKNOWN_ACTIONID, UNKNOWN_ISSUE, } = require('../utils/error.constants')
 
 
 const editController = async (request, response) => {
@@ -19,10 +19,27 @@ const editController = async (request, response) => {
       const id = body.elementId
       const editState = body.editState
       const currentUser = body.currentUser
-      Element.findOneAndUpdate({ elementId: id, whiteboardId: 1 }, { editState: editState, currentUser: currentUser }, function (err) {
+      Element.findOneAndUpdate({ elementId: id, whiteboardId: whiteboardId }, { editState: editState, currentUser: currentUser }, function (err) {
         if (err) throw new Error(err)
         else response.status(200).send('Element updated succesfully, updated element ID:' + id)
       })
+      break
+    }
+    case 7:{
+      const id = body.elementId
+      if(body.pos !== undefined && body.text !== undefined) {
+        const pos = body.pos
+        const text = body.text
+        Element.findOneAndUpdate({ elementId: id, whiteboardId: whiteboardId }, { text: text, pos: pos }, function (err) {
+          if (err) throw new Error(err)
+          else response.status(200).send('Element updated succesfully, updated element ID:' + id)
+        })
+      }
+      else{
+        throw new Error(UNKNOWN_ISSUE)
+      }
+
+
       break
     }
     default:
